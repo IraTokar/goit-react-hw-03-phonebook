b778bd496043defc5264767f2d162e9c63b841f2
IraTokar <tokar.ira20@gmail.com>
2025-05-23 14:28:14 +0200

    add validation for tel number

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ const storageKey = 'contacts';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Albus Dumbledore', number: '459-12-56' },
-      { id: 'id-2', name: 'Severus Snape', number: '443-89-12' },
-      { id: 'id-3', name: 'Minerva McGonagall', number: '645-17-79' },
-      { id: 'id-4', name: 'Remus Lupin', number: '227-91-26' },
+      { id: 'id-1', name: 'Albus Dumbledore', number: '3801826499' },
+      { id: 'id-2', name: 'Severus Snape', number: '+380974155597' },
+      { id: 'id-3', name: 'Minerva McGonagall', number: '+393296436625' },
+      { id: 'id-4', name: 'Remus Lupin', number: '3339503124' },
     ],
     filter: '',
   };
